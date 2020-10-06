961f5cace7e8d38fc382c2fb91e0959ec6cea2a8
Andrei Paleshchuk <alpoleshuck@gmail.com>
2020-10-07 01:28:48 +0300

    feat: add module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import Result from "../result/result";
+import Login from "../login/login";
+import Lose from "../lose/lose";
+import Artist from "../artist/artist";
+import Genre from "../genre/genre";
 import PropTypes from 'prop-types';
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={errorsCount} />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/result">
+          <Result />
+        </Route>
+        <Route exact path="/lose">
+          <Lose />
+        </Route>
+        <Route exact path="/dev-artist">
+          <Artist />
+        </Route>
+        <Route exact path="/dev-genre">
+          <Genre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 };
 
